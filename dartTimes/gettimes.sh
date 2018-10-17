b9b7f13c7bca6772282263f4969232abf42fbb98
Stefano Lupo <lupos@tcd.ie>
2018-10-17 22:34:33 +0100

    Added github API stuff

--- dartTimes/gettimes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash        
-chmod u+x gettimes.sh  
-
-echo "Checking Times.."
-wget -q -O scrapeResults.html 'http://www.irishrail.ie/timetables/live-departure-times?key=Sutton&REQ0JourneyStopskeyID=&HWAI=JS!js=yes&HWAI=JS!ajax=yes'
-python /home/stefano/bashScripts/webscraping/parse.py
-
-
- 	
